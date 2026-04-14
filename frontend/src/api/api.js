440c10e298a2bbb3597fa978241fff4de73f6400
Aniket8929 <aniketkumarrai271@gmail.com>
2026-04-14 12:59:14 +0530

    UI update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://library-management-hhze.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
